1cd79332f97413c2f63f699fc2eba9c8e186b162
Leo Brad <>
2023-11-01 22:26:11 +0800

    [Change] Use es6 template string.

--- src/server/lib/getHtml.js
+++ src/server/lib/getHtml.js
@@ -2,13 +2,13 @@ import handlebars from 'handlebars';
 import { minify, } from 'html-minifier';
 
 export default function getHtml(title, description, lang) {
-  const template = handlebars.compile(`
+  const template = `
     <!doctype html>
-    <html lang="{{lang}}">
+    <html lang="${lang}">
     <head>
     <meta charset="utf-8" />
-    <title>{{title}}</title>
-    <meta name="description" content="{{description}}" />
+    <title>${title}</title>
+    <meta name="description" content="${description}" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
@@ -16,8 +16,7 @@ export default function getHtml(title, description, lang) {
     <script src="main.bundle.js"></script>
     </body>
     </html>
-  `);
-  return minify(
-    template({ title, description, lang, }), { collapseWhitespace: true, },
+  `;
+  return minify(template, { collapseWhitespace: true, },
   );
 }
